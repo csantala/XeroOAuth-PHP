ad96621c67c164270fbc12e702e6aa971445af8d
Chris Santala <csantala@gmail.com>
2012-01-21 21:02:13 -0500

    added disclaimer to xero.php, added quick readme

--- xero/Xero.php
+++ xero/Xero.php
@@ -1,5 +1,26 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); 
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
+/**
+  * Xero.php
+  * 
+  * A Xero API authentication and usage library for Codeigniter.
+  * 
+  * Constructed from code by Ronan Quirke
+  * @link https://github.com/XeroAPI/XeroOAuth-PHP 
+  *
+  * @author Chris Santala <csantala@gmail.com>
+  * @author Ronan Quirke
+  *
+  * DISCLAIMER OF WARRANTY
+  * 
+  * This source code is provided "as is" and without warranties as to performance or merchantability. 
+  * The author and/or distributors of this source code may have made statements about this source code.
+  * Any such statements do not constitute warranties and shall not be relied on by the user in deciding whether to use this source code.
+  * This source code is provided without any express or implied warranties whatsoever. 
+  * Because of the diversity of conditions and hardware under which this source code may be used, no warranty of fitness for a particular purpose is offered.
+  * The user is advised to test the source code thoroughly before relying on it. The user must assume the entire risk of using the source code. Have fun.
+  */
+ 
 require('OAuthSimple.php');
 require('Xro_config.php');
 
